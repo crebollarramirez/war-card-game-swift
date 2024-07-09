413ededb472aeaa86239786af64bf0d5b8f565e0
Chris <christopherrebollar0@icloud.com>
2024-07-09 16:31:24 -0700

    Initial Commit

---
+++ war card game swift/war_card_game_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_game_swiftApp.swift
+//  war card game swift
+//
+//  Created by Christopher Rebollar on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct war_card_game_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
